6ff1ae8d825fab05c3b436251ec366badecc7c91
HermersonDev <ne.hermerson@gmail.com>
2020-03-22 16:37:31 -0300

    :construnction: create the signin saga action

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,32 @@
-import { all } from 'redux-saga/effects';
+import {
+  all, takeLatest, call, put,
+} from 'redux-saga/effects';
 
-export default all([]);
+import api from '../../../services/api';
+import history from '../../../services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, '/sessions', {
+    email,
+    password,
+  });
+
+  const { user, token } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Nâo é um provider');
+    return;
+  }
+
+  yield put(signInSuccess(user, token));
+
+  history.push('/dashboard');
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
