45739c89a0d2b4115bd698d498402dd5255cbf4f
HermersonDev <ne.hermerson@gmail.com>
2020-03-22 16:36:46 -0300

    :construction: create the signin actions

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,25 @@
+export function signInRequest(email, password) {
+  return {
+    type: '@auth/SIGN_IN_REQUEST',
+    payload: {
+      email,
+      password,
+    },
+  };
+}
+
+export function signInSuccess(user, token) {
+  return {
+    type: '@auth/SIGN_IN_SUCCESS',
+    payload: {
+      token,
+      user,
+    },
+  };
+}
+
+export function signFailure() {
+  return {
+    type: '@auth/SIGN_FAILURE',
+  };
+}
