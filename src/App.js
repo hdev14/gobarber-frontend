0f3e3d6dd10a61102f1262cc6e68d9737ba7224a
HermersonDev <ne.hermerson@gmail.com>
2020-03-22 16:40:12 -0300

    :construction: update the route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './configs/ReactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
 
-import './configs/ReactotronConfig';
 import store from './store';
 
 import GlobaStyle from './styles/global';
