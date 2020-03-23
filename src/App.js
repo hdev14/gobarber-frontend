11ef7c0f64046280d70f673c7bf2370ad9189483
HermersonDev <ne.hermerson@gmail.com>
2020-03-22 22:29:10 -0300

    :construction: add the ToastContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import './configs/ReactotronConfig';
 
@@ -17,6 +18,7 @@ export default function App() {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Router history={history}>
+          <ToastContainer autoClose={3000} />
           <GlobaStyle />
           <Routes />
         </Router>
