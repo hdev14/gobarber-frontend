32c2c1e9af880b10651a815b1798728d1de561b4
HermersonDev <ne.hermerson@gmail.com>
2020-03-23 14:57:07 -0300

    :construction: create the action and saga methods for register of user

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -33,6 +33,26 @@ export function* signIn({ payload }) {
   }
 }
 
+export function* signUp({ payload }) {
+  const { name, email, password } = payload;
+
+  try {
+    yield call(api.post, '/users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('Cadastro realizado com sucesso!');
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha ao cadastrar, por favor verifique seus dados!');
+    history.push('/register');
+  }
+}
+
 export default all([
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
