a9acfddb1bf848a89431d5a5a83cc1663f8ca6c0
HermersonDev <ne.hermerson@gmail.com>
2020-03-25 16:55:54 -0300

    :construction: :lipstick: update the profile component

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { UnForm, UnInput, PrimaryButton } from '../../styles/utils';
+
+import { ProfileContainer, Line, SecondaryButton } from './styles';
 
 export default function Profile() {
-  return <h1>profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleOnSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <ProfileContainer>
+      <UnForm initialData={profile} onSubmit={handleOnSubmit}>
+        <UnInput type="text" name="name" placeholder="Seu nome" />
+        <UnInput type="email" name="email" placeholder="Seu email" />
+        <Line />
+        <UnInput type="password" name="oldPassword" placeholder="Senha atual" />
+        <UnInput type="password" name="newPassword" placeholder="Nova senha" />
+        <UnInput type="password" name="confimPassword" placeholder="Confirmar nova senha" />
+        <PrimaryButton type="submit">atualizar perfil</PrimaryButton>
+      </UnForm>
+      <SecondaryButton type="button">Sair</SecondaryButton>
+    </ProfileContainer>
+  );
 }
