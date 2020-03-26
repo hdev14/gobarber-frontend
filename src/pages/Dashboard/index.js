a9beeada8759cf8da0fb38e7159709e710aa812b
HermersonDev <ne.hermerson@gmail.com>
2020-03-26 16:09:32 -0300

    :lipstick: create the style to dashboard component

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
-import api from '../../services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { DashboardContainer, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('/appointments');
-  return <h1>dash</h1>;
+  return (
+    <DashboardContainer>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#f4991a" />
+        </button>
+        <strong>26 Março</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#f4991a" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Hermerson Araújo</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Hermerson Araújo</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Hermerson Araújo</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Hermerson Araújo</span>
+        </Time>
+      </ul>
+    </DashboardContainer>
+  );
 }
